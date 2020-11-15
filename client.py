626421bd3dff477eaa7a31119798482d0245a5d2
Mattias Tofte <mattiastofte@gmail.com>
2020-11-15 02:42:30 +0100

    added multi-threading to client for recieving messages from server

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import socket
+import threading
+import time
+from os import system
+import sys
 
 HEADER = 64
 PORT = 65432
@@ -6,6 +10,37 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "46.9.226.154"
 ADDR = (SERVER, PORT)
+VERSION = "Client"
+DEVELOPER = "github.com/mattiastofte/"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+system("title "+VERSION)
+
+def send(message):
+    message = message.encode(FORMAT) # encodes to utf-8
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT) # sends a string with length of message
+    send_length += b' '*(HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+def receive():
+    print("recieve thread online")
+    while True:
+        message_length = client.recv(HEADER).decode(FORMAT) # decode from bytes format to string using utf-8
+        if message_length: # first message is empty (null), to tell that the client is connected, therefor you have to check if message_length has content. 
+            message_length = int(message_length)
+            message = client.recv(message_length).decode(FORMAT)
+            print(f"\r{message}")
+
+def start():
+    print(f"[Client] Succesfully connected to server ")
+    x = threading.Thread(target=receive, args=())
+    x.start()
+    while True:
+        send(input(""))
+
+print(f"simple-chat\nmade with love <3 by {DEVELOPER}\n")
+start()
+    
